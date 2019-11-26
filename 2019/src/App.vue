15ce53012e21ebb179eb0f621d56977e7b401862
Innei <tukon479@gmail.com>
2019-11-26 15:53:11 +0800

    init

---
+++ 2019/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+::-webkit-scrollbar
+  height 0
+  width 0
+</style>
---
+++ 2019/src/components/FullPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page"></div>
+</template>
+
+<style lang="stylus" scoped>
+.page
+  height 100vh
+  width 100vw
+</style>
